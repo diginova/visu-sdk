d0a675482f280eb80cef1db8fad03e565611e979
emin olmez <e.olmez42@gmail.com>
2022-09-04 01:55:37 +0300

    upt:pydantic model

--- base/Request.py
+++ base/Request.py
@@ -1,14 +1,16 @@
 
 import json
 from sdks.visu.base.Model import Model
+from sdks.visu.Image.base64 import decode64
+import numpy as np
 
 class Request:
     def __init__(self, jsondata):
         self.data = json.loads(jsondata)
         self.model=""
         
-    def get(self,name):
-        return self.model.name
+    def get(self):
+        return  np.asarray(decode64(self.model.components[0].inputs.image.imageData)).astype(np.uint8)
         
 
     
